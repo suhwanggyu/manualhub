00fe349be66e1f54400e90f34ffb63222fe12746
이종법 <bub3690@naver.com>
2019-10-21 15:09:28 +0000

    new header , footer ,contents

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		hello3
+	</div>
+
+
+</template>
+
+<script>
+
+	
+	
+
+export default {
+  name: 'MainFooter',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
