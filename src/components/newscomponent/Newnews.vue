a7873dc4a31bb757075a495abd589484e8a71b57
wang <wang1@hanyang.ac.kr>
2019-10-24 16:05:58 +0900

    finished

--- src/components/newscomponent/Newnews.vue
+++ src/components/newscomponent/Newnews.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="NewNews">
-        <el-table id="newsTable" border :default-sort = "{prop: 'date', order: 'ascending'}" :data="textLog" @row-click="clickLog" style="width: 100%">
+        <el-table id="newsTable" border :default-sort = "{prop: 'date', order: 'descending'}" :data="textLog" @row-click="clickLog" style="width: 100%">
             <el-table-column label="NEWS : 새로운 기록 또는 최근 수정된 기록 열람">
             <el-table-column :span="12" prop="comment" label="title" min-width="260"></el-table-column>
             <el-table-column :span="6" prop="author" label="등록자" min-width="100"></el-table-column>
@@ -26,7 +26,7 @@ export default {
     methods:{
         clickLog(receiver_id){
                 this.$store.state.selected = receiver_id.index;
-                this.$store.state.historyIndex = receiver_id.history_index
+                this.$store.state.historyIndex = receiver_id.history_ndex
                 //히스트 설정
                 this.$store.commit(FIND_INDEX)
                 this.$store.commit(CLICK_LIST)
